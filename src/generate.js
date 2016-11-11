315d97f781ac26d85791ef1faa4821f1b40d809a
Derek <fiveflip@gmail.com>
2016-11-11 11:53:16 -0800

    init

---
+++ src/generate.js
@@ -0,0 +1,4 @@
+export default function (argv) {
+  log.verbose('Generate ran!')
+  log.debug('arguments', argv)
+}
---
+++ src/helpers/log.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-console, class-methods-use-this */
+import 'colors'
+import _ from 'lodash'
+
+const stringify = function stringify(obj) {
+  return JSON.stringify(obj, null, '  ')
+}
+
+export default class logger {
+  constructor(argv) {
+    this.opts = {}
+    this.opts.verbose = argv.verbose || false
+    this.opts.debug = argv['deep-debug']
+    _.bindAll(this, [
+      'debug',
+      'verbose',
+      'error',
+      'info',
+    ])
+  }
+  debug(msg, obj) {
+    if (this.opts.verbose) {
+      console.log(`${'DEBUG:'.red} ${msg}`)
+      if (obj) {
+        console.log(JSON.stringify(obj, null, '  '))
+      }
+    }
+  }
+  verbose(msg, obj) {
+    if (this.opts.verbose) {
+      console.log(`${'VERBOSE:'.blue} ${msg}`)
+      if (obj) {
+        console.log(stringify(obj, null, '  '))
+      }
+    }
+  }
+  error(msg, err) {
+    console.log(`${'ERROR:'.red} ${msg}`)
+    if (err) {
+      console.error(err)
+    }
+  }
+  info(msg, data) {
+    console.log(`${'INFO:'.cyan} ${msg}`)
+    if (data) {
+      console.log(stringify(data))
+    }
+  }
+}
